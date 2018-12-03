1fe05ab421aaf6192a9a11bae404d6a092f3f1fb
zakaria ziouziou <zakaria.Ziouziou@metropolia.fi>
2018-12-03 16:24:23 +0200

    e

--- app/src/main/java/com/example/test1111/DisplayActivity.java
+++ app/src/main/java/com/example/test1111/DisplayActivity.java
@@ -1,13 +1,30 @@
 package com.example.test1111;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class DisplayActivity extends AppCompatActivity {
 
+  TextView showtext1;
+  TextView showtext2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        Intent intent = getIntent();
+
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        int number = intent.getIntExtra(MainActivity.EXTRA_NUMBER, 0);
+         showtext1 = findViewById(R.id.textView4);
+        showtext2= findViewById(R.id.textView5);
+       showtext1.setText(message);
+       showtext2.setText("" + number);
+
     }
 }
--- app/src/main/java/com/example/test1111/MainActivity.java
+++ app/src/main/java/com/example/test1111/MainActivity.java
@@ -4,20 +4,30 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.task4_1.Message";
+    public static final String EXTRA_NUMBER ="com.example.task4_1.number";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void sendMessage(View vew){
-        Intent intent = new Intent(this, DisplayActivity.class);
 
+        EditText editText =(EditText) findViewById(R.id.editText);
+        String  message = editText.getText().toString();
+        EditText editText2 = (EditText) findViewById(R.id.editText2);
+        int number = Integer.parseInt(editText2.getText().toString());
+        Intent intent =new Intent(this, DisplayActivity.class);
+        intent.putExtra(EXTRA_MESSAGE, message);
+        intent.putExtra(EXTRA_NUMBER,number);
         startActivity(intent);
+
+
     }
 }
