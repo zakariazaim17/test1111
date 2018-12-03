fde3e479a5fe7c01bec7d55f4e9775ba8335c753
zakaria ziouziou <zakaria.Ziouziou@metropolia.fi>
2018-12-03 15:26:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/test1111/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.test1111;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View vew){
+        Intent intent = new Intent(this, DisplayActivity.class);
+
+        startActivity(intent);
+    }
+}
